9e8461ec86f13e8e2af79ffe6f24e0dc07c0fd5d
Rogério Gastão <eltrondemais@gmail.com>
2024-01-11 16:35:50 +0100

    select box added

--- pages/book_reviews.py
+++ pages/book_reviews.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import pandas as pd
 
-st.write("teste")
+df_reviews = pd.read_csv("datasets/customer reviews.csv")
+df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
+
+books = df_top100_books["book title"].unique()
+book = st.sidebar.selectbox("Book", books)
+
+book
