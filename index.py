572774693f374b8bac1898d3e6145dce7766859b
Rogério Gastão <eltrondemais@gmail.com>
2024-01-11 03:58:12 +0100

    pausa

--- index.py
+++ index.py
@@ -1,5 +1,11 @@
 # Build a book dashboard
 
 import streamlit as st
+import pandas as pd
 
-st.write("Hello World")
+st.set_page_config(layout="wide")
+
+df_reviews = pd.read_csv("datasets/customer reviews.csv")
+df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
+
+df_reviews
