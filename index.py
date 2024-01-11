7707537327b0f246b772b8b64754b67628d35e5c
Rogério Gastão <eltrondemais@gmail.com>
2024-01-11 15:26:14 +0100

    chart added

--- index.py
+++ index.py
@@ -1,11 +1,22 @@
-# Build a book dashboard
+# Build an Amazon Top books dashboard
 
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 
 df_reviews = pd.read_csv("datasets/customer reviews.csv")
 df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
 
-df_reviews
+price_max = df_top100_books["book price"].max()
+price_min = df_top100_books["book price"].min()
+
+max_price = st.sidebar.slider("Price Range", price_min, price_max, price_max)
+df_books = df_top100_books[df_top100_books["book price"] <= max_price]
+
+df_books
+
+fig = px.bar(df_books["year of publication"].value_counts())
+
+st.plotly_chart(fig)
