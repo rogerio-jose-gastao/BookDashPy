237a0cf13b48371a31f1693714eb2bff02c527cb
Rogério Gastão <eltrondemais@gmail.com>
2024-01-12 13:54:34 +0100

    commited

--- pages/book_reviews.py
+++ pages/book_reviews.py
@@ -9,14 +9,8 @@ df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
 books = df_top100_books["book title"].unique()
 book = st.sidebar.selectbox("Book", books)
 
-df_book = df_top100_books[df_reviews["book name"] == book]
+df_book = df_top100_books[df_top100_books["book title"] == book]
+df_reviews_f = df_reviews[df_reviews["book name"] == book]
 
-book_title = df_book["book title"].iloc[0]
-book_gender = df_book["gender"].iloc[0]
-book_price = df_book["book price"].iloc[0]
-book_rating = df_book["rating"].iloc[0]
-book_year = df_book["year of publication"].iloc[0]
-
-# df_book
-# df_reviews
+df_reviews_f
 
