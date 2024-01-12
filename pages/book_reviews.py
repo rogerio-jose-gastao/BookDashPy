274c4417e1226b177a038e501c5465c58fdc4252
Rogério Gastão <eltrondemais@gmail.com>
2024-01-12 13:02:00 +0100

    new

--- pages/book_reviews.py
+++ pages/book_reviews.py
@@ -1,10 +1,22 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout="wide")
+
 df_reviews = pd.read_csv("datasets/customer reviews.csv")
 df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
 
 books = df_top100_books["book title"].unique()
 book = st.sidebar.selectbox("Book", books)
 
-book
+df_book = df_top100_books[df_reviews["book name"] == book]
+
+book_title = df_book["book title"].iloc[0]
+book_gender = df_book["gender"].iloc[0]
+book_price = df_book["book price"].iloc[0]
+book_rating = df_book["rating"].iloc[0]
+book_year = df_book["year of publication"].iloc[0]
+
+# df_book
+# df_reviews
+
